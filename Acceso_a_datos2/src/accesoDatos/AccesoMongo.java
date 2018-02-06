ebd6de0b452d81e038200f41655b4073660239fc
Sergio Redondo Montes <sergio.redondo@live.u-tad.com>
2018-02-06 12:42:21 +0100

    actualizar okey

--- Acceso_a_datos2/src/accesoDatos/AccesoMongo.java
+++ Acceso_a_datos2/src/accesoDatos/AccesoMongo.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
-import org.bson.BsonDocument;
 import org.bson.Document;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
@@ -18,6 +18,7 @@ import com.mongodb.client.model.ReturnDocument;
 import entidades.Alumno;
 import entidades.Titulacion;
 
+
 public class AccesoMongo implements I_Acceso_Datos {
 
 	MongoClient mongoClient;
@@ -25,7 +26,8 @@ public class AccesoMongo implements I_Acceso_Datos {
 	MongoDatabase db;
 	HashMap<String, Alumno> recogerAlumnos;
 	HashMap<String, Titulacion> recogerTitulaciones;
-	int mayorCod;
+	int mayorCodAlumno;
+	int mayorCodTitu;
 
 	public AccesoMongo() {
 		try {
@@ -62,25 +64,27 @@ public class AccesoMongo implements I_Acceso_Datos {
 
 		try {
 
-//			// PASO 3: Obtenemos una coleccion para trabajar con ella
-//			collection = db.getCollection("titulaciones");
-//
-//			// PASO 4.2.1: "READ" -> Leemos todos los documentos de la base de
-//			// datos
-//			int numDocumentos = (int) collection.count();
-//			System.out.println("Número de documentos (registros) en la colección alumnos: " + numDocumentos + "\n");
-//
-//			BsonDocument documentBson;
-//			Document alumnoDocument = null;
+			// // PASO 3: Obtenemos una coleccion para trabajar con ella
+			// collection = db.getCollection("titulaciones");
+			//
+			// // PASO 4.2.1: "READ" -> Leemos todos los documentos de la base
+			// de
+			// // datos
+			// int numDocumentos = (int) collection.count();
+			// System.out.println("Número de documentos (registros) en la
+			// colección alumnos: " + numDocumentos + "\n");
+			//
+			// BsonDocument documentBson;
+			// Document alumnoDocument = null;
 
 			for (Entry<String, Titulacion> entry : recogerTitulaciones.entrySet()) {
 				String key = entry.getKey();
 				Titulacion titulacion = entry.getValue();
 				ArrayList<Document> arrAlumno = titulacion.getArrayAlumnos();
-				for (int i = 0; i <arrAlumno.size(); i++) {
+				for (int i = 0; i < arrAlumno.size(); i++) {
 					cod = arrAlumno.get(i).getInteger("cod");
-					if (cod>=mayorCod) {
-						mayorCod=cod;
+					if (cod >= mayorCodAlumno) {
+						mayorCodAlumno = cod;
 					}
 					nombre = arrAlumno.get(i).getString("nombre");
 					dni = arrAlumno.get(i).getString("dni");
@@ -112,17 +116,17 @@ public class AccesoMongo implements I_Acceso_Datos {
 	@Override
 	public boolean insertarAlumno(Alumno alumno) {
 		boolean todoOK = true;
-		
+
 		try {
-			 
+
 			collection = db.getCollection("titulaciones");
-			
+
 			// Para que salga ordenado el hashmap de monedas (de stackoverflow)
 			Document updatedDocument = collection.findOneAndUpdate(
-		    		Filters.eq("titulacion", alumno.getTitulacionAlumno().getNombre()),
-		    		new Document("$push",alumnoToDocument(alumno)),
-		    		new FindOneAndUpdateOptions().returnDocument(ReturnDocument.AFTER));
-			
+					Filters.eq("titulacion", alumno.getTitulacionAlumno().getNombre()),
+					new Document("$push", alumnoToDocument(alumno, "insert")),
+					new FindOneAndUpdateOptions().returnDocument(ReturnDocument.AFTER));
+
 			recogerAlumnos.put(alumno.getDni(), alumno);
 
 		} catch (Exception e) {
@@ -132,13 +136,71 @@ public class AccesoMongo implements I_Acceso_Datos {
 		}
 
 		return todoOK;
-		
+
 	}
 
 	@Override
 	public boolean borrarAlumno(String dni) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		boolean todoOK = true;
+
+		try {
+
+			collection = db.getCollection("titulaciones");
+			String titulacion = recogerAlumnos.get(dni).getTitulacionAlumno().getNombre();
+
+			
+			Document filtro = new Document();
+			filtro.put("titulacion", titulacion);
+			filtro.put("alumnos.dni", dni);
+			System.out.println(filtro);
+			
+			Document delete = new Document();
+			delete.put("dni", dni);
+			
+			Document update = new Document();
+			update.put("$pull", delete);
+			
+			System.out.println(update);
+			collection.updateOne(filtro,update);
+
+		} catch (Exception e) {
+			todoOK = false;
+			System.out.println("Opcion guardar datos de Depositos no disponible por el momento");
+			e.printStackTrace();
+		}
+
+		return todoOK;
+		
+		/*
+		boolean todoOK = true;
+
+		try {
+
+			collection = db.getCollection("titulaciones");
+			String titulacion = recogerAlumnos.get(dni).getTitulacionAlumno().getNombre();
+			ArrayList<Document> prueba = new ArrayList<Document>(recogerTitulaciones.get(titulacion).getArrayAlumnos());
+			
+			
+			Document filtro = new Document();
+			filtro.put("titulacion", titulacion);
+			filtro.put("alumnos.dni", dni);
+
+			Document update = new Document();
+			update.put("$pull", filtro);
+			System.out.println(update);
+			collection.updateOne(prueba,update);
+
+			recogerAlumnos.remove(dni);
+
+		} catch (Exception e) {
+			todoOK = false;
+			System.out.println("Opcion guardar datos de Depositos no disponible por el momento");
+			e.printStackTrace();
+		}
+
+		return todoOK;
+		*/
 	}
 
 	@Override
@@ -149,8 +211,38 @@ public class AccesoMongo implements I_Acceso_Datos {
 
 	@Override
 	public boolean actualizarAlumnos(Alumno alumno) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean todoOK = true;
+
+		try {
+
+			collection = db.getCollection("titulaciones");
+
+			// Para que salga ordenado el hashmap de monedas (de stackoverflow)
+			Document docAlumno = new Document();
+			docAlumno.put("alumnos.$.dni", alumno.getDni());
+			docAlumno.put("alumnos.$.nombre", alumno.getNombre());
+			docAlumno.put("alumnos.$.apellido", alumno.getApellido());
+			docAlumno.put("alumnos.$.telefono", alumno.getTelefono());
+			docAlumno.put("alumnos.$.nacionalidad", alumno.getNacionalidad());
+			docAlumno.put("alumnos.$.titulacionAlumno", alumno.getTitulacionAlumno().getNombre());
+			Document filtro = new Document();
+			filtro.put("titulacion", alumno.getTitulacionAlumno().getNombre());
+			filtro.put("alumnos.dni", alumno.getDni());
+
+			Document update = new Document();
+			update.put("$set", docAlumno);
+			System.out.println(update);
+			collection.updateOne(filtro, update);
+
+			recogerAlumnos.put(alumno.getDni(), alumno);
+
+		} catch (Exception e) {
+			todoOK = false;
+			System.out.println("Opcion guardar datos de Depositos no disponible por el momento");
+			e.printStackTrace();
+		}
+
+		return todoOK;
 	}
 
 	@Override
@@ -211,15 +303,15 @@ public class AccesoMongo implements I_Acceso_Datos {
 
 	@Override
 	public boolean insertarTitulacion(Titulacion titulacion) {
-boolean todoOK = true;
-		
+		boolean todoOK = true;
+
 		try {
-			 
+
 			collection = db.getCollection("titulaciones");
-			
+
 			// Para que salga ordenado el hashmap de monedas (de stackoverflow)
 			collection.insertOne(titulacionToDocument(titulacion));
-		    recogerTitulaciones.put(titulacion.getNombre(), titulacion);		
+			recogerTitulaciones.put(titulacion.getNombre(), titulacion);
 
 		} catch (Exception e) {
 			todoOK = false;
@@ -235,47 +327,52 @@ boolean todoOK = true;
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
-	private Document alumnoToDocument(Alumno auxAlum) {
-	    // Creamos una instancia Documento
-	    Document dbObjectAlumno = new Document();
-	    String titulacion = auxAlum.getTitulacionAlumno().getNombre();
-	    
-	  
-	    dbObjectAlumno.append("cod", ++mayorCod);
-	    dbObjectAlumno.append("dni", auxAlum.getDni());
-	    dbObjectAlumno.append("nombre", auxAlum.getNombre());
-	    dbObjectAlumno.append("apellido", auxAlum.getApellido());
-	    dbObjectAlumno.append("telefono", auxAlum.getTelefono());   
-	    dbObjectAlumno.append("nacionalidad", auxAlum.getNacionalidad());
-	    dbObjectAlumno.append("titulacionAlumno", titulacion);
-	    
-	    //documento titulacion para buscar donde insertar
-	    Document dbObjectTituInsertarAlumno = new Document();
-	    dbObjectTituInsertarAlumno.append("alumnos", dbObjectAlumno);
-	    
-	    
 
-	    return dbObjectTituInsertarAlumno;
-	    
-	    /*
-	     * Document updatedDocument = collection.findOneAndUpdate(
-					Filters.eq("idCurso", String.valueOf(alumno.getId_Curso())),
-					new Document("$push", new Document("alumnos", new Document(dbObjectDeposito))),
-					new FindOneAndUpdateOptions().returnDocument(ReturnDocument.AFTER));
-	     */
+	/*
+	 * @alumnoToDocument CAMBIAR METODO pasar por parametro si es insertar o
+	 * actualizar
+	 */
+	private Document alumnoToDocument(Alumno auxAlum, String tipo) {
+		// Creamos una instancia Documento
+		Document dbObjectAlumno = new Document();
+		String titulacion = auxAlum.getTitulacionAlumno().getNombre();
+
+		if (tipo.equals("insert")) {
+			dbObjectAlumno.append("cod", ++mayorCodAlumno);
+		} else {
+			dbObjectAlumno.append("cod", auxAlum.getCod());
+		}
+
+		// documento titulacion para buscar donde insertar
+
+		dbObjectAlumno.append("dni", auxAlum.getDni());
+		dbObjectAlumno.append("nombre", auxAlum.getNombre());
+		dbObjectAlumno.append("apellido", auxAlum.getApellido());
+		dbObjectAlumno.append("telefono", auxAlum.getTelefono());
+		dbObjectAlumno.append("nacionalidad", auxAlum.getNacionalidad());
+		dbObjectAlumno.append("titulacionAlumno", titulacion);
+		Document dbObjectTituInsertarAlumno = new Document();
+		dbObjectTituInsertarAlumno.append("alumnos", dbObjectAlumno);
+		return dbObjectTituInsertarAlumno;
+
+		/*
+		 * Document updatedDocument = collection.findOneAndUpdate(
+		 * Filters.eq("idCurso", String.valueOf(alumno.getId_Curso())), new
+		 * Document("$push", new Document("alumnos", new
+		 * Document(dbObjectDeposito))), new
+		 * FindOneAndUpdateOptions().returnDocument(ReturnDocument.AFTER));
+		 */
 	}
-	
-	private Document titulacionToDocument(Titulacion auxTitu) {
-	    // Creamos una instancia Documento
-	    Document dbObjectTitulacion = new Document();
-	    ArrayList<Document> alumnosTitu = new ArrayList<>();
-	    dbObjectTitulacion.append("titulacion", auxTitu.getNombre());
-	    dbObjectTitulacion.append("descripcion", auxTitu.getDescripcion());
-	    dbObjectTitulacion.append("alumnos", alumnosTitu);
-	    
 
-	    return dbObjectTitulacion;
+	private Document titulacionToDocument(Titulacion auxTitu) {
+		// Creamos una instancia Documento
+		Document dbObjectTitulacion = new Document();
+		ArrayList<Document> alumnosTitu = new ArrayList<>();
+		dbObjectTitulacion.append("titulacion", auxTitu.getNombre());
+		dbObjectTitulacion.append("descripcion", auxTitu.getDescripcion());
+		dbObjectTitulacion.append("alumnos", alumnosTitu);
+
+		return dbObjectTitulacion;
 	}
 
 }
